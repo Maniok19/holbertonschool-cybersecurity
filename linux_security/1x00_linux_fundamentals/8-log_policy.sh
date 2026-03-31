60685807471dd1157d737896e709bc0dc7f14699
mano <ma@no.com>
2026-03-31 10:27:01 +0200

    task8

---
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p $1
+chown :$2 $1
+chmod 2750 "$1"
+cat << EOF > /etc/logrotate.d/app
+$1/*.log {
+    daily
+    rotate 7
+    missingok
+    notifempty
+    compress
+    delaycompress
+    create 0640 root $2
+}
+EOF
