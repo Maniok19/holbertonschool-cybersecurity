f5df8536c3b1ea8c79c6b0c83e0968b297c0fc68
mano <ma@no.com>
2026-03-31 10:16:11 +0200

    task7

--- linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
+++ linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+echo "#!/bin/bash
+cat /var/www/html/secret_config.php" > /usr/local/bin/audit-read-secret
+chown root:root /usr/local/bin/audit-read-secret
+chmod 755 /usr/local/bin/audit-read-secret
+
+echo "$1 ALL=(root) NOPASSWD: /usr/local/bin/audit-read-secret" > "/etc/sudoers.d/$1_access"
+chmod 440 /etc/sudoers.d/auditor_access
