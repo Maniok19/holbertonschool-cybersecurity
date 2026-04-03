5a2b806df818524e593d7fc5a5ca360307a1f24d
mano <ma@no.com>
2026-04-03 11:07:10 +0200

    3.4

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -5,10 +5,10 @@ apt-get install -y $1
 fi
 
 if grep -q "minclass" "$2";then
-    sed -i "s/.*minclass.*/minclass = 3/" "$2"
-    echo 'modified minclass = 3'
-    sed -i "s/.*minlen.*/minlen = 12/" "$2"
-    echo 'modified minlen = 12'
+    sed -i "s/.*minclass.*/minclass=3/" "$2"
+    echo 'modified minclass=3'
+    sed -i "s/.*minlen.*/minlen=12/" "$2"
+    echo 'modified minlen=12'
 else
     echo "minlen = 14" >> "$2"
     echo "minclass = 3" >> "$2"
