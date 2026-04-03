75cf575434cc12b059cefece4d91b01c077496ec
mano <ma@no.com>
2026-04-03 13:04:25 +0200

    3.6

---
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+useradd -m $1
+usermod -L $1
+USER_HOME="/home/$1"
+mkdir "/home/$1/.ssh"
+chmod 700 "/home/$1/.ssh"
+echo "$2" > "/home/$1/.ssh/authorized_keys"
+chmod 600 "/home/$1/.ssh/authorized_keys"
+chown -R "$1:$1" "/home/$1/.ssh"
+passwd -S "$1" | awk '{print "passwd: password expiry information changed."}'
