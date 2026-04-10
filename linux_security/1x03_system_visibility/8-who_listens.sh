5d7cef95872a11909ed721051ca69746e42b2d12
mano <ma@no.com>
2026-04-10 15:27:51 +0200

    c

--- linux_security/1x03_system_visibility/8-who_listens.sh
+++ linux_security/1x03_system_visibility/8-who_listens.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsof -iTCP:$1 -sTCP:LISTEN -Fn | grep '^n' | head -n1 | cut -c2- | xargs -r basename -P
+lsof -iTCP:$1 -sTCP:LISTEN -Fn | grep '^n' | head -n1 | cut -c2- | xargs -r basename -P awk
