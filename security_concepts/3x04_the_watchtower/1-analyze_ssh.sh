72c802bf2462384fadc57675324b7e249ad477dd
Mano <mano19029@hotmail.fr>
2026-09-11 13:38:44 +0200

    tg

--- security_concepts/3x04_the_watchtower/1-analyze_ssh.sh
+++ security_concepts/3x04_the_watchtower/1-analyze_ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 LOG_FILE="$1"
-
 if [ ! -f "$LOG_FILE" ]; then
-    echo "Error: File '$LOG_FILE' not found."
-    exit 1
+echo "Error: File '$LOG_FILE' not found."
+exit 1
 fi
-grep "Failed password" "$LOG_FILE" | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr
+
+grep "Failed password" "$LOG_FILE" | grep -oE 'from [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}' | sort | uniq -c | sort -nr
