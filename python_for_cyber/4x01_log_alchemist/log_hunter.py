8b21b3cd7cb9578f3337cb0cf2c3668b558f7624
Maniok <mano19029@hotmail.fr>
2026-09-24 15:09:20 +0200

    gfn

--- python_for_cyber/4x01_log_alchemist/log_hunter.py
+++ python_for_cyber/4x01_log_alchemist/log_hunter.py
@@ -347,7 +347,7 @@ def main():
     parser.add_argument("file", help='Input file path')
     parser.add_argument("--report", help='Export alerts to a JSON file')
     parser.add_argument("--workers", type=int, default=0,
-                        help='Number of worker processes (0 = single-threaded)')
+                help='Number of worker processes (0 = single-threaded)')
     args = parser.parse_args()
 
     print("[*] LogHunter - Log Analysis Engine")
