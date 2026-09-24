598a5f6c97a87bc711a8e24f5a0be546632dad38
Maniok <mano19029@hotmail.fr>
2026-09-24 09:47:20 +0200

    ezr

--- python_for_cyber/4x00_python_security/breach_check.py
+++ python_for_cyber/4x00_python_security/breach_check.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(prog='Main',
+                                     description='The program is good',
+                                     epilog='End')
+    parser.add_argument("--file","-f", help='Input file path', required=True)
+    parser.add_argument("--verbose","-v", help='Print debug')
+    parser.add_argument("--output","-o", help='Output report file path')
+    args = parser.parse_args()
+
+
     print("BreachCheck v1.0 startup...")
 
 if __name__ == "__main__":
